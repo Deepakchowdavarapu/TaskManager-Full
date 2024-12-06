be37cc8aeb2defb8c19a9179ca6939d065ea60d4
Deepak Chowdavarapu <deepak.23bcs10092@ms.sst.scaler.com>
2024-12-06 19:05:45 +0530

    added port

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-dotenv.config()
-const mongoose = require('mongoose')
-const User = require('./Models/UserModel')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const Task = require('./Models/TaskModel')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const User = require('./Models/UserModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Task = require('./Models/TaskModel');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-    console.log(`connected to Mongo DataBase`)
-}).catch(()=>{
-    console.log(`Mongo connection failed`)
-})
+.then(() => {
+    console.log(`connected to Mongo DataBase`);
+}).catch(() => {
+    console.log(`Mongo connection failed`);
+});
 
 app.post('/auth/register', async (req, res) => {
     try {
@@ -237,10 +237,10 @@ app.get('/health' , async(req,res)=>{
     res.send("health")
 })
 
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
-// app.listen(port,()=>{ 
-//     console.log(`server is running on port ${process.env.PORT}`) 
-// })
+app.listen(port,()=>{ 
+    console.log(`server is running on port ${port}`) 
+})
 
-module.exports = app;
+// module.exports = app;
